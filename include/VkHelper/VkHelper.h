8667cdea8415cafbae2e79e80fd8344c0d1dcddf
OctoPro89 <robertvs8910@gmail.com>
2023-12-30 10:16:09 -0500

    Update stuff

--- include/VkHelper/VkHelper.h
+++ include/VkHelper/VkHelper.h
@@ -44,6 +44,8 @@ bool LoadFunctionExportedFromVulkanLoaderLibrary(const LIBRARY_TYPE* vulkan_libr
 
 	return true;
 }
+
+/* A function for loading global level vulkan functions */
 bool LoadGlobalLevelFunctions() {
 #define GLOBAL_LEVEL_VULKAN_FUNCTION( name )										\
     name = (PFN_##name)vkGetInstanceProcAddr( nullptr, #name );						\
@@ -113,6 +115,34 @@ bool CreateVulkanInstance(Vec ConstCharPointer_desired_extensions, const char* a
 	/* Doing the array / vector loop the old fashioned way because we can't use ranged based loop */
 	for (int i = 0; i < vec_length(available_extensions); ++i)
 	{
-		if(!IsExtensionSupported)
+		/* Loop through the available extensions to see if the desired ones are available */
+		VkExtensionProperties* extension = (VkExtensionProperties*)vec_get_at(ConstCharPointer_desired_extensions, i);
+		if (!IsExtensionSupported(available_extensions, extension))
+		{
+			printf("ERROR: Extension named \"%s\" is not supported but needed!", extension->extensionName);
+			return false;
+		}
+
+		VkApplicationInfo applicationInfo =
+		{
+			VK_STRUCTURE_TYPE_APPLICATION_INFO,
+			nullptr,
+			applicationName,
+			VK_MAKE_VERSION(1, 0, 0),
+			"nullpointer",
+			VK_MAKE_VERSION(1, 0, 0),
+			VK_MAKE_VERSION(1, 0, 0)
+		};
+
+		VkInstanceCreateInfo instanceCreateInfo =
+		{
+			VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+			nullptr,
+			0,
+			&applicationInfo,
+			0,
+			nullptr,
+			static_cast<uint32_t>();
+		};
 	}
 }
